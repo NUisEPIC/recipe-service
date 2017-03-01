32ed826314596999b45835c9d6ae04181d74bee3
Aaron Leon <aaronleon2018@u.northwestern.edu>
2017-02-28 20:48:09 -0600

    Clean directory and app structure

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -11,31 +10,17 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-
   req.method === 'OPTIONS' ? res.sendStatus(200) : next();
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/api', index);
-//app.use('/users', users);
 
 // catch 404 and forward to error handler
 //app.use(function(req, res, next) {
